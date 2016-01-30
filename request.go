6574ca83ab9ab9b267408c3b411145aca3a22386
MiskoLee <miskolee@weixinhost.com>
2016-01-30 12:06:53 +0800

    使用reflect机制实现类PHP的动态接口:
    从Action的角度来看,与编写本地函数无异,非侵入式的RPC调用

--- request.go
+++ request.go
@@ -1,7 +1,4 @@
 package yar
-import "fmt"
-
-
 
 type Request struct {
 	Protocol *Protocol   `json:"-" msgpack:"-"`
@@ -9,47 +6,3 @@ type Request struct {
 	Method   string      `json:"m" msgpack:"m"`
 	Params   interface{} `json:"p" msgpack:"p"`
 }
-
-
-
-
-func (self *Request)GetParamWithInt32(index int)(ret int32){
-
-	fmt.Println(self.Params)
-
-	return int32(self.Params.([]interface{})[index].(float64))
-
-}
-
-func (self *Request)GetParamWithInt64(index int)(ret int64){
-
-	return int64(self.Params.([]interface{})[index].(float64))
-
-}
-
-func (self *Request)GetParamWithBool(index int)(ret bool){
-
-	return bool(self.Params.([]interface{})[index].(bool))
-
-}
-
-
-func (self *Request)GetParamWithString(index int)(ret string){
-
-	return string(self.Params.([]interface{})[index].(string))
-
-}
-
-func (self *Request)GetParamWithArray(index int)(ret []interface{}) {
-
-	return self.Params.([]interface{})[index].([]interface{})
-
-}
-
-
-func (self *Request)GetParamWithMap(index int)(ret map[string]interface{}) {
-
-	return self.Params.([]interface{})[index].(map[string]interface{})
-
-}
-
