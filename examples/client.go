4325f80960161f842fd829256ef4a9baacdcac86
MiskoLee <miskolee@weixinhost.com>
2016-01-30 16:22:38 +0800

    add example snowflake server

--- examples/client.go
+++ examples/client.go
@@ -6,15 +6,16 @@ import (
 
 func main(){
 
-	client ,err := yar.NewClientWithTcp("127.0.0.1",6789)
+	client ,err := yar.NewClientWithTcp("127.0.0.1",6790)
 
 	if err != nil {
 		fmt.Printf("err:%s",err)
 		return
 	}
 
-	var ret string
-	err = client.Call("test",&ret,123,1.23,"abcdefg")
+	var ret map[string]uint64
+//	err = client.Call("test",&ret,123,1.23,"abcdefg")
+	err = client.Call("uuid",&ret)
 	fmt.Println(ret)
 	fmt.Println(err)
 }
