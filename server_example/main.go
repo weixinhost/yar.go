0a7d9078db02a05fec4683f8179534fe9af26096
misko <misko@miskodeMacBook-Pro.local>
2017-02-07 11:39:42 +0800

    2.0 md

--- server_example/main.go
+++ server_example/main.go
@@ -1,34 +1,20 @@
 package main
 
-import (
-	"io/ioutil"
-	"log"
-	"net/http"
-
-	"github.com/weixinhost/yar.go"
-	"github.com/weixinhost/yar.go/server"
-)
+import "github.com/weixinhost/yar.go/server"
 
 type YarClass struct{}
 
-func (c *YarClass) Echo() string {
-	log.Println("echo handler")
-	return "string"
+func (c *YarClass) Echo(str interface{}) interface{} {
+	return str
 }
 
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		body, _ := ioutil.ReadAll(r.Body)
-		s := server.NewServer(&YarClass{})
-		s.Opt.MagicNumber = yar.MagicNumber
-		s.Opt.LogLevel = yar.LogLevelDebug | yar.LoglevelNormal | yar.LogLevelError
-		s.Opt.LogLevel = yar.LogLevelDebug
-		s.Register("echo", "Echo")
-		//这里接收完整的request body与一个 Writer，可以和任意web框架结合
-		//数据通过Writer进行写回
-		s.Handle(body, w)
-	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	//构建一个Yar Server
+	yar := server.NewServer(&YarClass{})
+	//基于FastHttp 构建的Server
+	httpServer := server.NewHttpServer()
+	//注册路由
+	httpServer.RegisterHandle("/", yar)
+	//启动Serve
+	httpServer.Serve(":8080")
 }
